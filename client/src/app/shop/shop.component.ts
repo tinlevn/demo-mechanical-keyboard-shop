1e948a21d5bbf3a4b106da9a66f3f4f6baa5bdcb
Tin Le <thanhtin.le.vn@gmail.com>
2022-02-09 22:07:34 -0500

    General UI features added and changed default product page size to 15

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Brand } from '../shared/models/brand';
 import { Product } from '../shared/models/product';
+import { Type } from '../shared/models/productType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -9,15 +11,38 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: Product[];
+  brands: Brand[];
+  types: Type[];
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit() {
+    this.getProducts();
+    this.getBrands();
+    this.getProductTypes();
+  }
+  getProducts(){
     this.shopService.getProducts().subscribe(response =>{
       this.products= response.data;
+      }, error=>{
+        console.log(error);
+    });
+  }
+
+  getBrands(){
+    this.shopService.getBrands().subscribe(response=>{
+      this.brands=response;
     }, error=>{
       console.log(error);
-    });
+  });
+  }
+
+  getProductTypes(){
+    this.shopService.getTypes().subscribe(response=>{
+      this.types=response;
+    }, error=>{
+      console.log(error);
+  });
   }
 
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pagination } from '../shared/models/pagination';
+import {Brand} from '../shared/models/brand';
+import {Type} from '../shared/models/productType';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +16,10 @@ export class ShopService {
     return this.http.get<Pagination>(this.baseUrl+ 'products');
   }
 
+  getBrands(){
+    return this.http.get<Brand[]>(this.baseUrl+'products/brands');
+  }
+  getTypes(){
+    return this.http.get<Type[]>(this.baseUrl+'products/types');
+  }
 }
